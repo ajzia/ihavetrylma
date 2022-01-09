84a4736fd6fa6bb967726afb3eda0729defae725
ajzia <jokul502@wp.pl>
2022-01-10 00:39:04 +0100

    WinningConditions class that checks if Player won

---
+++ src/main/java/Server/WinningConditions.java
@@ -0,0 +1,40 @@
+package Server;
+
+import java.util.ArrayList;
+
+public class WinningConditions {
+
+    private final int side;
+    private final ArrayList<GameTile> pawns;
+
+    protected WinningConditions(int side, ArrayList<GameTile> pawns) {
+        this.side = side;
+        this.pawns = pawns;
+
+        for (GameTile p: pawns) {
+            System.out.println(p);
+        }
+
+    }
+
+    protected int howManyPawns() {
+        int pieces = 0;
+        for (int i = 1; i < side; i++) {
+            pieces += i;
+        }
+        return pieces;
+    }
+
+    protected boolean ifWon() {
+        int inBase = 0;
+
+        for (GameTile p : pawns) {
+            if (p.getOwner() == p.getBase()) {
+                inBase++;
+            }
+        }
+
+        System.out.println("sprawdzamy");
+        return inBase == howManyPawns();
+    }
+}
