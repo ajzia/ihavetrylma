bb433bc704eeeb186cf82ff2ea2f203262c79057
sana1319 <sandra.szwed2211@gmail.com>
2022-01-09 10:38:50 +0100

    Fixed: adding class for button

---
+++ src/main/java/com/example/ihavetrylma/SkipButton.java
@@ -0,0 +1,13 @@
+package com.example.ihavetrylma;
+
+import javafx.scene.control.Button;
+
+public class SkipButton extends Button {
+
+    public SkipButton(){
+        setText("Skip");
+        setVisible(true);
+
+        //setOnMouseClicked();
+    }
+}
