9a0805c2731667101d4fd5589d3718530b47ce96
ajzia <jokul502@wp.pl>
2022-01-11 00:57:09 +0100

    MoveValidationTest added

---
+++ src/test/java/Server/GameRules/MoveValidationTest.java
@@ -0,0 +1,32 @@
+package Server.GameRules;
+
+import Server.GameBoard;
+import Server.GameTile;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MoveValidationTest {
+
+    MoveValidation mov;
+    GameBoard gameBoard;
+    GameTile[][] tile;
+
+    @BeforeEach
+    void setUp() {
+        int side = 5;
+        int height = side + 3 * (side - 1);
+        int width = 2 * (side + 2 * (side - 1)) - 1;
+        int goalPlayers = 3;
+
+        tile = new GameTile[width][height];
+        gameBoard = new GameBoard(width, height, tile, goalPlayers, side);
+    }
+
+    @Test
+    public void isValidTest() {
+        mov = new MoveValidation(5, 5, 7, 5, 5, -1, tile);
+        Assertions.assertEquals(2, mov.isValid());
+    }
+
+}
