bd5108a2e831d277a966377b01212c7ae9e5b121
ajzia <jokul502@wp.pl>
2022-01-10 16:04:17 +0100

    Checking for blockades

--- src/main/java/Server/GameRules/Blockade.java
+++ src/main/java/Server/GameRules/Blockade.java
@@ -1,4 +1,6 @@
-package Server;
+package Server.GameRules;
+
+import Server.GameTile;
 
 import java.util.ArrayList;
 
@@ -8,58 +10,39 @@ public class Blockade {
     protected final int width;
     protected final int height;
     protected final int side;
+    protected final int pieces;
     protected final int players;
+    protected ArrayList<GameTile> blockades = new ArrayList<>();
 
-    protected Blockade(GameTile[][] tiles, int width, int height, int side, int players) {
+    public Blockade(GameTile[][] tiles, int width, int height, int side, int pieces, int players) {
         this.tiles = tiles;
         this.width = width;
         this.height = height;
         this.side = side;
+        this.pieces = pieces;
         this.players = players;
     }
-    
-    protected ArrayList<GameTile> setBlockades(ArrayList<GameTile> blockades) {
 
-        switch (players) {
-            case 2 -> {
-                firstBlockade(blockades);
-                fourthBlockade(blockades);
-            }
-            case 3 -> {
-                secondBlockade(blockades);
-                fourthBlockade(blockades);
-                sixthBlockade(blockades);
-            }
-            case 4 -> {
-                secondBlockade(blockades);
-                thirdBlockade(blockades);
-                fifthBlockade(blockades);
-                sixthBlockade(blockades);
-            }
-            case 6 -> {
-                firstBlockade(blockades);
-                secondBlockade(blockades);
-                thirdBlockade(blockades);
-                fourthBlockade(blockades);
-                fifthBlockade(blockades);
-                sixthBlockade(blockades);
-            }
-        }
-
-        return blockades;
+    public void setBlockades() {
+        firstBlockade();
+        secondBlockade();
+        thirdBlockade();
+        fourthBlockade();
+        fifthBlockade();
+        sixthBlockade();
     }
 
-    private void firstBlockade(ArrayList<GameTile> blockades) {
+    private void firstBlockade() {
         int temp = width / 2 - (side - 3);
         for (int i = side - 3; i <= side - 2; i++) {
-            for (int j = 0; j <= i; j ++) {
+            for (int j = 0; j <= i; j++) {
                 blockades.add(tiles[temp + 2 * j][i]);
             }
             temp--;
         }
     }
 
-    private void secondBlockade(ArrayList<GameTile> blockades) {
+    private void secondBlockade() {
         int x = width - 1 - 2 * (side - 2);
         int y = side - 1;
         for (int i = 0; i < side - 1; i++) {
@@ -77,7 +60,7 @@ public class Blockade {
         }
     }
 
-    private void thirdBlockade(ArrayList<GameTile> blockades) {
+    private void thirdBlockade() {
         int x = width - 1 - 2 * (side - 2);
         int y = height - side;
         for (int i = 0; i < side - 1; i++) {
@@ -96,17 +79,17 @@ public class Blockade {
 
     }
 
-    private void fourthBlockade(ArrayList<GameTile> blockades) {
+    private void fourthBlockade() {
         int temp = width / 2 - (side - 3);
         for (int i = height - (side - 2); i > height - side; i--) {
-            for (int j = 0; j < height - i; j ++) {
+            for (int j = 0; j < height - i; j++) {
                 blockades.add(tiles[temp + 2 * j][i]);
             }
             temp--;
         }
     }
 
-    private void fifthBlockade(ArrayList<GameTile> blockades) {
+    private void fifthBlockade() {
         int x = 2 * (side - 2);
         int y = height - side;
         for (int i = 0; i < side - 1; i++) {
@@ -124,7 +107,7 @@ public class Blockade {
         }
     }
 
-    private void sixthBlockade(ArrayList<GameTile> blockades) {
+    private void sixthBlockade() {
         int x = 2 * (side - 2);
         int y = side - 1;
         for (int i = 0; i < side - 1; i++) {
@@ -140,6 +123,31 @@ public class Blockade {
             x--;
             y++;
         }
+    }
+
+    public boolean isBlockade(ArrayList<GameTile> triangle, int owner) {
+        int blocks = 2 * side - 3;
+        int fields = (owner + 3) % 6;
+        int blocked = 0;
+
+        for (int i = blocks * fields; i < blocks * (fields + 1); i++) {
+            if (blockades.get(i).getOwner() == blockades.get(i).getBase()) {
+                blocked++;
+            }
+        }
+
+        if (blocked == blocks) {
+            int occupied = 0;
+            for (GameTile tile : triangle) {
+                if (tile.hasOwner()) {
+                    occupied++;
+                }
+            }
+
+            return occupied == pieces;
+        }
+
+        return false;
     }
 
 }
