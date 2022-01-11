d7b075a4b79b44fda8389c295de9ea2b8ce95a96
sana1319 <sandra.szwed2211@gmail.com>
2022-01-11 06:33:36 +0100

    BoardGUITest added

---
+++ src/test/java/Client/BoardGUITest.java
@@ -0,0 +1,46 @@
+package Client;
+
+import javafx.application.Platform;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+public class BoardGUITest {
+
+    @Test
+    public void heightTest() {
+        BoardGUI boardGUI = new BoardGUI();
+        boardGUI.calculateHeight(4);
+        Assertions.assertEquals(13, boardGUI.getHeight());
+
+        boardGUI.calculateHeight(5);
+        Assertions.assertEquals(17, boardGUI.getHeight());
+
+        boardGUI.calculateHeight(6);
+        Assertions.assertEquals(21, boardGUI.getHeight());
+    }
+
+    @Test
+    public void widthTest(){
+        BoardGUI boardGUI = new BoardGUI();
+        boardGUI.calculateWidth(4);
+        Assertions.assertEquals(19, boardGUI.getWidth());
+
+        boardGUI.calculateWidth(5);
+        Assertions.assertEquals(25, boardGUI.getWidth());
+
+        boardGUI.calculateWidth(6);
+        Assertions.assertEquals(31, boardGUI.getWidth());
+    }
+
+    @Test
+    public void createBoardTest() throws InterruptedException {
+        final BoardGUI boardGUI = new BoardGUI();
+        Platform.startup(() -> boardGUI.createBoard());
+        CountDownLatch cdl = new CountDownLatch(1);
+        Platform.runLater(cdl::countDown);
+        cdl.await();
+    }
+
+}
